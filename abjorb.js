b4b16480f76aba9e548c519b44aee38a76001dee
Philip Hazelden <philip.hazelden@gmail.com>
2010-11-26 21:52:57 +0000

    Cell moves when you click.

--- abjorb.js
+++ abjorb.js
@@ -5,26 +5,31 @@ var player;
 function abjorb () {
     G = new Global();
 
-    player = new Cell(10, 50, 20);
+    player = new Cell($V([320, 240]), $V([0,0]), 20);
 
     setInterval(mainloop, 1000/60);
+
+    $(document).click(function(e){player.clickHandler(e)});
 }
 
 function mainloop () {
     G.context.clearRect(0, 0, G.canvas.width, G.canvas.height);
 
     player.update();
+    G.context.fillStyle = "rgb(200, 200, 200)";
+    G.context.fillRect(0, 0, G.canvas.width, G.canvas.height);
     player.draw();
 }
 
-function Cell (x, y, radius) {
-    this.x = x;
-    this.y = y;
+function Cell (pos, vel, radius) {
+    this.pos = pos;
+    this.vel = vel;
+
     this.radius = radius;
 }
 
 Cell.prototype.update = function () {
-    this.x += 10;
+    this.pos = this.pos.add(this.vel);
 }
 
 Cell.prototype.draw = function () {
@@ -32,7 +37,16 @@ Cell.prototype.draw = function () {
 
     ctx.fillStyle = "rgb(200, 0, 0)";
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+    ctx.arc(this.pos.e(1), this.pos.e(2), this.radius, 0, Math.PI*2, true);
     ctx.closePath();
     ctx.fill();
+}
+
+Cell.prototype.clickHandler = function (e) {
+    var loc = $V([e.clientX - G.canvas.offsetLeft,
+		  e.clientY - G.canvas.offsetTop]);
+
+    var offset = this.pos.subtract(loc);
+
+    this.vel = this.vel.add(offset.toUnitVector().x(1/2));
 }
--- global.js
+++ global.js
@@ -2,5 +2,3 @@ function Global () {
     this.canvas = $("canvas")[0];
     this.context = this.canvas.getContext("2d");
 }
-
-G = new Global;
